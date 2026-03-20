cee82f248a49e6cb135bd3776b636c4bad8daf56
rajaraovarre <mrrajaraovarre143@gmail.com>
2026-03-20 11:57:26 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ data "aws_availability_zones" "available" {
 }
 
 locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs = slice(data.aws_availability_zones.available.names, 0, 3)    #syntax: slice(list, start, end)
 }
 
 # ---- VPC ----
